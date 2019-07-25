4575fe33d6a057d3f023deb75a99cc4aeee43d58
Jinil Jang <jinil@nyu.edu>
2019-07-25 00:14:35 -0700

    Adding 2 solutions

---
+++ 0311_Sparse_Matrix_Multiplication.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def multiply(self, A, B):
+        """
+        :type A: List[List[int]]
+        :type B: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        output = [[0] * len(B[0]) for _ in range(len(A))]
+        for i in range(len(A)):
+            for j in range(len(B[0])):
+                for k in range(len(A[0])):
+                    output[i][j] += A[i][k] * B[k][j]
+        return output
